8dcd8cd84bb4ce16c4ce702ccfd890b4a216cd27
smannaba <74249644+rahmads773@users.noreply.github.com>
2022-01-26 22:42:56 +0100

    Update time.js

--- time.js
+++ time.js
@@ -17,3 +17,31 @@ year.innerHTML = date.getFullYear();
 }
 setInterval(date1, 100);
 onload = date1();
+// dark mode
+const sun = document.getElementById("sun")
+const moon = document.getElementById("moon")
+const body = document.querySelector("body")
+const mcontainer = document.querySelector(".container-mode")
+
+moon.addEventListener("click", (eo) => {
+  mode("dark");
+  localStorage.setItem("theme", "dark");
+});
+sun.addEventListener("click", (eo) => {
+  mode("light");
+  localStorage.setItem("theme", "light");
+});
+onload = (eo) => {
+  mode(localStorage.getItem("theme"));
+};
+const mode = (id) => {
+  if (id === "dark") {
+    moon.style.opacity ="0";
+    sun.style.opacity ="1";
+    body.classList.add("dark");
+  }else if(id === "light"){
+    moon.style.opacity ="1";
+    sun.style.opacity ="0";
+    body.classList.remove("dark");
+  }
+}
